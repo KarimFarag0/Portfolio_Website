bfa9a21cfacfec2c1c948258749ad4a1d9e60424
Karim_F <karim.michelf@gmail.com>
2024-11-09 14:15:00 -0500

    Updated CV Doc

--- personal-portfolio/src/components/about/Info.jsx
+++ personal-portfolio/src/components/about/Info.jsx
@@ -14,7 +14,7 @@ const Info = () => {
         <i class='bx bx-briefcase-alt about__icon'></i>
 
             <h3 className="about__title">Completed</h3>
-            <span className="about__subtitle">3 projects</span>
+            <span className="about__subtitle">5 projects and counting</span>
         </div>
 
         <div className="about__box">
