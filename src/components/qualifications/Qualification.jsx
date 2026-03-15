f449bda92086175a51c25614d29fcf36842bada0
Karim_F <karim.michelf@gmail.com>
2026-03-15 17:18:10 -0400

    Typo Errors

--- src/components/qualifications/Qualification.jsx
+++ src/components/qualifications/Qualification.jsx
@@ -12,7 +12,7 @@ const Qualification = () => {
   return (
     <section className="qualification section">
         <h2 className="section__title">Qualification</h2>
-        <span className="section__subtitle">My peronal journey</span>
+        <span className="section__subtitle">My personal journey</span>
 
 
         <div className="qualification__container container">
